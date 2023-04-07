154fb2d2894c23eab38f9c49ed6287bacf0218da
Pavel <pvlwrk@gmail.com>
2023-04-07 12:59:53 +0300

    add ListProducts component

---
+++ src/components/UI/ListProducts/ListProducts.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <div class="list-products">
+    <label for="">Название товара</label>
+    <label for="">Количество</label>
+    <label for="">Стоимость</label>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style> 
