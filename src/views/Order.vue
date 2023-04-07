c9e214165c686507541ba4150d73b8c3fe1197d9
Pavel <pvlwrk@gmail.com>
2023-04-07 12:58:08 +0300

    add Order view

---
+++ src/views/Order.vue
@@ -0,0 +1,30 @@
+<script setup>
+import SelectFood from '../components/UI/SelectFood/SelectFood.vue';
+import QuantityFood from '../components/UI/QuantityFood/QuantityFood.vue';
+import AddFoodBtn from '../components/UI/AddFoodBtn/AddFoodBtn.vue';
+import ListProducts from '../components/UI/ListProducts/ListProducts.vue';
+
+
+function onClickAddBtn() {
+}
+
+</script>
+
+
+<template>
+  <div>
+    <header class="header"></header>
+    <SelectFood />
+    <QuantityFood />
+    <AddFoodBtn @on-click-add-btn="onClickAddBtn" />
+    <ListProducts />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  height: 60px;
+  width: 100%;
+  background-color: #755858;
+}
+</style>
