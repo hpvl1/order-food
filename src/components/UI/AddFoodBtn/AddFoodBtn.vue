c1d47cfec89ae328935f8e559adc0a3638a58889
Pavel <pvlwrk@gmail.com>
2023-04-07 12:59:24 +0300

    add AddFoodBtn component

---
+++ src/components/UI/AddFoodBtn/AddFoodBtn.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <div class="add-food">
+    <button @click="$emit('on-click-add-btn')">Добавить</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.add-food > button {
+  width: 395px;
+  height: 55px;
+  margin: 40px 0px;
+  background: #2FA6EA;
+}
+</style>
